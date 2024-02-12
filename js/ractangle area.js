ac3914fb0a65d2b700f0d6b23583dd24cadf68e4
sakibsazal <sakibsazal@gmail.com>
2024-02-13 00:30:58 +0600

    add this commit

--- js/ractangle area.js
+++ js/ractangle area.js
@@ -1,3 +1,17 @@
 function calculateRectangleArea(){
-    
+    const widthInput = document.getElementById('rectangle-width');
+   const rectangleWidthText = widthInput.value;
+    const width = parseFloat(rectangleWidthText);
+    console.log( width );
+
+    const lengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = lengthInput.value;
+    const length = parseFloat(rectangleLengthText);
+    console.log(length);
+
+    const area = width*length;
+    console.log('Area of the rectangle is:',area);
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
